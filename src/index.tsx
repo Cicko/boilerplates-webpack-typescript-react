203ad522f066ff18598f65c0d18ae5da51aba95d
Rudolf <rudolfcickodev@gmail.com>
2020-11-07 17:56:15 +0100

    First commig

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+
+const App = () => {
+
+    return <div>Hola amigo mio</div>
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+)
